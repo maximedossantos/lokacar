6dacba28cec699ad0fcaa53203a0f7fa34800d58
mdossantos <maxime.dossantos01@gmail.com>
2017-05-02 17:02:13 +0200

    Class Categorie.java

---
+++ app/src/main/java/fr/projet/lokacar/models/Categorie.java
@@ -0,0 +1,19 @@
+package fr.projet.lokacar.models;
+
+/**
+ * Created by mdossantos2015 on 02/05/2017.
+ */
+
+public class Categorie {
+
+    private String nom;
+    
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+}
