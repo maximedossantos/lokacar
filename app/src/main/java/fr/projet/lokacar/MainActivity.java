6cbd2d40588496a739078ba7889b4c44068f1965
DOS SANTOS Maxime <maxime.dossantos2015@campus-eni.fr>
2017-05-02 16:14:54 +0200

    Dev Main Activity et création de l'activité home

--- app/src/main/java/fr/projet/lokacar/MainActivity.java
+++ app/src/main/java/fr/projet/lokacar/MainActivity.java
@@ -1,13 +1,31 @@
 package fr.projet.lokacar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Timer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+
+            }
+        }, 2000);
+
     }
 }
